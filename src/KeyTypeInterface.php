c4706f778b9e41eca366c32d0dde1d81a55d3be4
Kris <kris@meridian-ds.com>
2015-07-14 14:09:10 -0500

    removing unnecessary plugin methods

--- src/KeyTypeInterface.php
+++ src/KeyTypeInterface.php
@@ -15,17 +15,6 @@ use Drupal\Core\Plugin\PluginFormInterface;
  * Provides an interface defining a Key Type plugin.
  */
 interface KeyTypeInterface extends PluginInspectionInterface, ConfigurablePluginInterface, PluginFormInterface {
-  /**
-   * Returns a translated string for the constraint title.
-   * @return string
-   */
-  public function getTitle();
-
-  /**
-   * Returns a translated description for the constraint description.
-   * @return string
-   */
-  public function getDescription();
 
   /**
    * Returns the value of a key from the key type.

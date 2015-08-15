2224e5150e314431787b5d18a441a8999f21d721
Matthew Radcliffe <mradcliffe@softpixel.com>
2015-08-15 12:58:09 -0400

    Add explicit coverage of Key::getKeyValue method instead of relying on KeyManagerTest.

--- tests/src/Entity/KeyEntityTest.php
+++ tests/src/Entity/KeyEntityTest.php
@@ -7,6 +7,7 @@
 namespace Drupal\Tests\key\Entity;
 
 use Drupal\key\Entity\Key;
+use Drupal\key\Plugin\KeyProvider\SimpleKey;
 use Drupal\Tests\key\KeyTestBase;
 
 /**
@@ -15,6 +16,12 @@ use Drupal\Tests\key\KeyTestBase;
  * @coversDefaultClass \Drupal\key\Entity\Key
  */
 class KeyEntityTest extends KeyTestBase {
+
+  /**
+   * @var []
+   *   Key settings to use for SimpleKey provider.
+   */
+  protected $key_settings;
 
   /**
    * Assert that key entity getters work.
@@ -26,11 +33,46 @@ class KeyEntityTest extends KeyTestBase {
     $values = [
       'key_id' => $this->getRandomGenerator()->word(15),
       'key_provider' => 'key_provider_simple',
-      'key_settings' => ['simple_key_value' => $this->createToken()]
+      'key_settings' => $this->key_settings,
     ];
     $key = new Key($values, 'key');
 
     $this->assertEquals($values['key_provider'], $key->getKeyProvider());
     $this->assertEquals($values['key_settings'], $key->getKeySettings());
+    $this->assertEquals($values['key_settings']['simple_key_value'], $key->getKeyValue());
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $definition = [
+      'id' => 'key_provider_simple',
+      'title' => 'Simple Key',
+      'storage_method' => 'config'
+    ];
+    $this->key_settings = ['simple_key_value' => $this->createToken()];
+    $plugin = new SimpleKey($this->key_settings, 'key_provider_simple', $definition);
+
+    // Mock the KeyProviderPluginManager service.
+    $this->KeyProviderManager = $this->getMockBuilder('\Drupal\key\KeyProviderPluginManager')
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    $this->KeyProviderManager->expects($this->any())
+      ->method('getDefinitions')
+      ->willReturn([
+        ['id' => 'key_provider_file', 'title' => 'File Key', 'storage_method' => 'file'],
+        ['id' => 'key_provider_simple', 'title' => 'Simple Key', 'storage_method' => 'config']
+      ]);
+    $this->KeyProviderManager->expects($this->any())
+      ->method('createInstance')
+      ->with('key_provider_simple', $this->key_settings)
+      ->willReturn($plugin);
+    $this->container->set('plugin.manager.key.key_provider', $this->KeyProviderManager);
+
+    \Drupal::setContainer($this->container);
   }
 }
